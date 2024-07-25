856dc2009a2e559cec908b0eee7017bf32b16be4
vinayak <vcharms@gmail.com>
2024-07-25 17:11:35 +0530

    Added new files Demo.java and .gitignore

---
+++ Demo.java
@@ -0,0 +1,5 @@
+public class Demo {
+	public void m1(){
+		System.out.println("This is m1 method");
+	}
+}
--- Test.java
+++ Test.java
@@ -2,5 +2,8 @@ public class Test{
 	public static void main(String []args){
 		System.out.println("Hello World");
 		System.out.println("Git is a version control system");
+
+		Demo d = new Demo();
+		d.m1();
 	}
 }
